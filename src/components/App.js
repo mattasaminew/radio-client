45a41c40536ed243ad015a1408ed079932eee425
Matthew Asaminew <mattasaminew@gmail.com>
2018-04-12 19:42:07 -0700

    Add play toggle, no audio

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,51 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
+import recordLogo from '../img/logo-record.png';
 import '../css/App.css';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			playing: false
+		};
+
+		this.togglePlay = this.togglePlay.bind(this);
+	}
+	togglePlay() {
+		this.setState((prevState) => { return {
+			playing: !prevState.playing
+		}})
+	}
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+				<div className="app-container">
+					<div className="app-header">
+						ቀንድ
+					</div>
+					<AudioPlayer
+						playing={this.state.playing}
+						togglePlay={this.togglePlay}
+					/>
+				</div>
       </div>
     );
   }
+}
+
+class AudioPlayer extends Component {
+	render() {
+		return(
+			<div className='audio-player'>
+				<img src={recordLogo} className={this.props.playing ? "player-indicator active" : "player-indicator"} alt="logo" />
+				<div className="play-button" onClick={this.props.togglePlay}>
+					{this.props.playing ? 'Pause' : 'Play'}
+				</div>
+			</div>
+		);
+
+	}
 }
 
 export default App;
